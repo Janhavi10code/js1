176ad4ea7b09354fdc7855564d37bb17b5835f82
Janhavi10code <diwatejanhvi10@gmail.com>
2025-07-04 10:04:53 +0000

    add

---
+++ index.js
@@ -0,0 +1,34 @@
+//var state={
+var taskList = [
+  {
+    imageUrl: "",
+    taskTitle: "",
+    taskType: "",
+    taskDescription: "",
+  },
+  {
+    imageUrl: "",
+    taskTitle: "",
+    taskType: "",
+    taskDescription: "",
+  },
+  {
+    imageUrl: "",
+    taskTitle: "",
+    taskType: "",
+    taskDescription: "",
+  },
+  {
+    imageUrl: "",
+    taskTitle: "",
+    taskType: "",
+    taskDescription: "",
+  },
+  {
+    imageUrl: "",
+    taskTitle: "",
+    taskType: "",
+    taskDescription: "",
+  },
+];
+// };
