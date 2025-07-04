2b05da0b1d6a9db47d4af31225bca8ef098ace4a
Janhavi10code <diwatejanhvi10@gmail.com>
2025-07-04 12:02:52 +0000

    add

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
-//var state={
-var taskList = [
-  {
-    imageUrl: "",
-    taskTitle: "",
-    taskType: "",
-    taskDescription: "",
-  },
-  {
-    imageUrl: "",
-    taskTitle: "",
-    taskType: "",
-    taskDescription: "",
-  },
-  {
-    imageUrl: "",
-    taskTitle: "",
-    taskType: "",
-    taskDescription: "",
-  },
-  {
-    imageUrl: "",
-    taskTitle: "",
-    taskType: "",
-    taskDescription: "",
-  },
-  {
-    imageUrl: "",
-    taskTitle: "",
-    taskType: "",
-    taskDescription: "",
-  },
-];
+// var state={
+//    taskList :[
+//   {
+//     imageUrl: "",
+//     taskTitle: "",
+//     taskType: "",
+//     taskDescription: "",
+//   },
+//   {
+//     imageUrl: "",
+//     taskTitle: "",
+//     taskType: "",
+//     taskDescription: "",
+//   },
+//   {
+//     imageUrl: "",
+//     taskTitle: "",
+//     taskType: "",
+//     taskDescription: "",
+//   },
+//   {
+//     imageUrl: "",
+//     taskTitle: "",
+//     taskType: "",
+//     taskDescription: "",
+//   },
+//   {
+//     imageUrl: "",
+//     taskTitle: "",
+//     taskType: "",
+//     taskDescription: "",
+//   },
+// ],
+
 // };
+
+const state = {
+  taskList: [],
+};
+//Dom operation
+const taskContents = document.querySelector("task_contents");
+const taskModal = document.querySelector("task_modal_body");
+
+// console.log(taskContents);
+// console.log("taskModal");
+const htmlTaskcontent = ({ id, title, description, task }) => {};
